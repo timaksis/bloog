5dd9622e3772d9e38eadb695939f233d20bcd2f0
timaksis <47370705+timaksis@users.noreply.github.com>
2019-02-06 01:51:41 +0300

    Add files via upload

---
+++ application/views/main/about.php
@@ -0,0 +1,19 @@
+<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Обо мне</h1>
+                    <span class="subheading">БГПУ им. М. Танка</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>Студент </p>
+        </div>
+    </div>
+</div>
